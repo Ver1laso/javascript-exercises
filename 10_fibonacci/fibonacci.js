7d23d531e804c9fbb7539abc23152c324280b4ea
verilaso <69420275+Ver1laso@users.noreply.github.com>
2024-06-28 22:37:26 +0100

    all test unlocked

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+
+    fib = parseInt(fib);
+
+    
+    if (fib < 0 ) return "OOPS";
+    if (fib === 0) return 0;
+    if (fib === 1) return 1;
+
+    let n1 = 0, n2 = 1;
+
+    for(let i=2 ; i < fib ; i++){
+
+        let n3 = n2 + n1
+        n1 = n2;
+        n2 = n3;
+    }
+    
+    return n1 + n2;
 
 };
 
