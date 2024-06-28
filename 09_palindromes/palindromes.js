a3e058880539030f01de6ff48e9e9599d954ac11
verilaso <69420275+Ver1laso@users.noreply.github.com>
2024-06-28 22:01:24 +0100

    all test unlocked

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    var cleanedStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    var splitString = cleanedStr.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    if(cleanedStr === joinArray){
+        return true;
+    }else {
+        return false;
+    }
 };
 
 // Do not edit below this line
