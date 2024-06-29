0fb882ac49dd037fbb196b06902f09fdbe850128
verilaso <69420275+Ver1laso@users.noreply.github.com>
2024-06-29 16:23:52 +0100

    all test unlocked

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,33 @@
-const findTheOldest = function() {
+function checkBirthAndDeath (person) {
+    return person.hasOwnProperty('yearOfBirth') && person.hasOwnProperty('yearOfDeath');
+}
 
+
+
+
+const findTheOldest = function(people) {
+
+    let oldestPerson = {name: '', age: 0};
+
+    for (let i = 0; i < people.length ; i++){
+        if(checkBirthAndDeath(people[i])){
+            let age = people[i].yearOfDeath - people[i].yearOfBirth;
+
+            if(age > oldestPerson.age){
+                oldestPerson.name = people[i].name;
+                oldestPerson.age = age;
+            }
+        }else {
+            let currentYear = 2024;
+            let age = currentYear - people[i].yearOfBirth;
+            if(age > oldestPerson.age){
+                oldestPerson.name = people[i].name;
+                oldestPerson.age = age;
+            }
+        }
+    }
+    return oldestPerson;
+    
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
